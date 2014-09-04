6f310135032f0f20e9ee7ea90e47e9b7273bca68
Avol <avolaso@gmail.com>
2014-09-04 15:56:56 +0200

    alpha

---
+++ alpha 0.1/alpha 0.1/Assets/Source/RoomScripts/Clickable/ClickableObjectScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Clickable object script is attached to all interactive Objects.
+/// </summary>
+
+public class ClickableObjectScript : MonoBehaviour 
+{
+	public Material normal;
+	public Material iluminated;
+	
+	private string name;
+	private float play;
+	private ParticleEmitter particle;
+	
+	
+	void Awake () 
+	{
+		particle = this.gameObject.transform.FindChild("SparkleParticles").GetComponent<ParticleEmitter>();
+	}
+	
+	void Start ()
+	{
+		particle.emit = false;
+	}
+	
+	void Update () 
+	{
+		play = Mathf.PingPong(Time.time, 3);
+		if (play > 2.9)
+		{
+			particle.emit = true	;		
+		}
+		else
+		{
+			particle.emit = false;
+		}
+
+	}
+	
+	void OnMouseOver () 
+	{	
+		this.gameObject.renderer.material = iluminated;
+	}
+	
+	void OnMouseExit ()
+	{
+		this.gameObject.renderer.material = normal;
+	}
+	
+	virtual public void OnMouseDown ()
+	{
+		// To be overridden
+	}
+}
